e2b929b553639467250070d56cfd74517e7188d9
dperry233 <freemail40@yahoo.com>
2017-06-05 19:17:33 +0300

    add list_batch function to main file

--- my_list.c
+++ my_list.c
@@ -452,6 +452,51 @@ int list_compute(linked_list_t* list, int key, int (*compute_func) (void *), int
 	return NOT_EX_ERROR;
 }
 
+
+typedef struct op_wrapper_t
+{
+	linked_list_t* list;
+	op_t* op;
+} op_wrapper_t;
+
+
+/**
+ * wrapper function to be used for pthread_create in list_batch
+ */
+void* batch_wrapper(void* param){
+	linked_list_t* list= (linked_list_t*)(param->op);
+	op_t* curr_op= (op_t*)(param->op);
+	int current_key = ((curr_op)->key
+
+		if(curr_op->op==INSERT){
+			curr_op->result =  list_insert(list, current_key, (curr_op->data));
+		}
+
+		if((curr_op)->op==REMOVE){
+			curr_op->result =  list_remove(list, current_key);
+		}
+
+		if(curr_op->op==CONTAINS){
+			curr_op->result = list_find(list, current_key);
+		}
+
+		if(curr_op->op==UPDATE){
+			curr_op->result = list_update(list, current_key, curr_op->data);
+			
+		}
+		int compute_result;
+		if(curr_op->op==COMPUTE){
+			curr_op->result = list_compute(list,current_key,curr_op->compute_func, &compute_result);
+			curr_op->data = (void*)(long long)compute_result;
+		}
+
+		free(param);
+}
+
+
+
+
+
 /**
  * list_batch : Performs a several different operations on the list.
  *
@@ -472,7 +517,28 @@ int list_compute(linked_list_t* list, int key, int (*compute_func) (void *), int
  *
  * return value	: 0 in case of success or anything else in case of failure.
  */
-void list_batch(linked_list_t* list, int num_ops, op_t* ops);
+void list_batch(linked_list_t* list, int num_ops, op_t* ops){
+	if (!list||num_ops<0||!ops){
+		return PARAM_ERROR;
+	}
+	int i;
+	pthread_t threads[num_ops];
+	for(i=0;i<num_ops;i++){
+		
+
+		op_wrapper_t* wrapper=(int *)malloc(sizeof(*op_wrapper_t));
+		wrapper->list= list;
+		wrapper->op=ops+i;
+		pthread_create(&threads[i], NULL,batch_wrapper, (void*)wrapper);
+	}
+	for(i=0;i<num_ops;i++){
+		pthread_join(threads[i], NULL);
+	}
+
+
+	return 0; 
+
+}
 
 
 
